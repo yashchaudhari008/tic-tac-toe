6cdc7d7a4865cb53f1e7df37c17036017bde17fe
Yash Chaudhari <yashchaudhari008@gmail.com>
2022-07-13 16:58:58 +0530

    Added fullscreen button. (#4)

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import screenfull from "screenfull";
 import "./Footer.css";
+import { MdFullscreen, MdFullscreenExit } from "react-icons/md";
 
 export default function Footer() {
+	const [isFullscreen, setFullscreen] = useState(screenfull.isFullscreen);
+	const handleFullscreen = () => {
+		setFullscreen((old) => !old);
+		screenfull.toggle();
+	};
 	return (
-		<div id="footer">
-			Made with ❤️ by madOverGames |{" "}
-			<a href="https://github.com/madOverGames/tic-tac-toe">Github</a>
-		</div>
+		<>
+			<div className="quickSettings">
+				{screenfull.isEnabled &&
+					(!isFullscreen ? (
+						<MdFullscreen onClick={handleFullscreen} />
+					) : (
+						<MdFullscreenExit onClick={handleFullscreen} />
+					))}
+			</div>
+			<div id="footer">
+				Made with ❤️ by madOverGames |{" "}
+				<a href="https://github.com/madOverGames/tic-tac-toe">Github</a>
+			</div>
+		</>
 	);
 }
