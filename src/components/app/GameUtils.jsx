28ce25740ada53bea6e108e1cb7d9444388271dc
Yash Eksambekar <yasheksambekar@gmail.com>
2022-07-13 20:00:20 +0530

    minified minimax logic

--- src/components/app/GameUtils.jsx
+++ src/components/app/GameUtils.jsx
@@ -39,39 +39,27 @@ export const checkWinner = (board) => {
 export const nextMove = (board, alpha, turn) => {
 	const minimax = (board, alpha, turn) => {
 		const scores = {
-			x: alpha === "x" ? 10 : -10,
-			o: alpha === "o" ? 10 : -10,
+			x: alpha === "x" ? 1 : -1,
+			o: alpha === "o" ? 1 : -1,
 			t: 0,
 		};
 		const result = checkWinner(board);
 		if (result !== null) return scores[result];
-		if (turn === alpha) {
-			let score = -Infinity;
-			for (let i = 0; i < 3; i++) {
-				for (let j = 0; j < 3; j++) {
-					if (board[i][j] === null) {
-						board[i][j] = turn;
-						let newScore = minimax(board, alpha, turn === "x" ? "o" : "x");
-						board[i][j] = null;
-						score = Math.max(score, newScore);
-					}
-				}
-			}
-			return score;
-		} else {
-			let score = Infinity;
-			for (let i = 0; i < 3; i++) {
-				for (let j = 0; j < 3; j++) {
-					if (board[i][j] === null) {
-						board[i][j] = turn;
-						let newScore = minimax(board, alpha, turn === "x" ? "o" : "x");
-						board[i][j] = null;
-						score = Math.min(score, newScore);
-					}
-				}
-			}
-			return score;
-		}
+		let score = turn === alpha ? -Infinity : Infinity;
+		for (let i = 0; i < 3; i++)
+			for (let j = 0; j < 3; j++)
+				if (board[i][j] === null)
+					score = (turn === alpha ? Math.max : Math.min)(
+						score,
+						minimax(
+							board.map((row, m) =>
+								row.map((col, n) => (m === i && n === j ? turn : col))
+							),
+							alpha,
+							turn === "x" ? "o" : "x"
+						)
+					);
+		return score;
 	};
 	let bestScore = -Infinity;
 	let bestMove = { i: null, j: null };
@@ -79,10 +67,14 @@ export const nextMove = (board, alpha, turn) => {
 		for (let j = 0; j < 3; j++) {
 			// Is spot available?
 			if (board[i][j] === null) {
-				// Play that spot
-				board[i][j] = turn;
-				let score = minimax(board, alpha, turn === "x" ? "o" : "x");
-				board[i][j] = null;
+				// Play that spot and get score
+				let score = minimax(
+					board.map((row, m) =>
+						row.map((col, n) => (m === i && n === j ? turn : col))
+					),
+					alpha,
+					turn === "x" ? "o" : "x"
+				);
 				if (score > bestScore) {
 					bestScore = score;
 					bestMove = { i, j };
