4a5a6675598f8409cf08fbc9d385cb1270534d34
Yash Chaudhari <yashchaudhari008@gmail.com>
2022-07-16 12:44:47 +0530

    Added install button (#8)

--- src/components/app/MainMenu.jsx
+++ src/components/app/MainMenu.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaPlay, FaRobot } from "react-icons/fa";
+import { FiDownload } from "react-icons/fi";
 import Button from "../ui/Button";
 import "./MainMenu.css";
 
@@ -12,6 +13,16 @@ export default function MainMenu({ setGameState }) {
 	const openGameView = (gameMode) => {
 		setGameState(gameMode);
 	};
+
+	const [installEvent, setInstallEvent] = useState(null);
+	useEffect(() => {
+		const handler = (event) => {
+			event.preventDefault();
+			setInstallEvent(event);
+		};
+		window.addEventListener("beforeinstallprompt", handler);
+		return () => window.removeEventListener("beforeinstallprompt", handler);
+	}, []);
 	return (
 		<div className="mainMenu">
 			<h1 className="heading">Tic-Tac-Toe</h1>
@@ -27,6 +38,20 @@ export default function MainMenu({ setGameState }) {
 					onClickHandler={() => openGameView(gameModes.computer)}
 				/>
 			</div>
+			{installEvent !== null && (
+				<Button
+					iconComponent={<FiDownload strokeWidth={3} />}
+					buttonText={"Install"}
+					onClickHandler={() => {
+						installEvent.prompt().then((event) => {
+							if (event.outcome === "accepted") {
+								setInstallEvent(null);
+							}
+						});
+					}}
+					secondaryBtn={true}
+				/>
+			)}
 		</div>
 	);
 }
